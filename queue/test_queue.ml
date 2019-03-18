f125bd232b2c2c8c9420ea46a87281f247091d18
KC Sivaramakrishnan <kc@kcsrk.info>
2019-03-18 19:09:18 +0000

    Added fuzzing for queue

--- queue/test_queue.ml
+++ queue/test_queue.ml
@@ -7,7 +7,7 @@ module U = struct
 end
 
 module IntAtom = struct
-    type t = int
+  type t = int [@@deriving crowbar]
     let compare = Pervasives.compare
     (* User defined merges for atom values *)
     let resolve x y = '#'
@@ -16,6 +16,27 @@ module IntAtom = struct
     (* Used for presentation purposes *)
     let to_string = string_of_int
   end
+
+(* Queue *)
+let _ =
+  U.print_header "Queue";
+  let module M = Queue_imp.Make(IntAtom) in
+
+  let a = M.empty |> M.add 4 |> M.add 3 |> M.add 2 |> M.add 1  in
+  let b = M.empty |> M.add 4 |> M.add 1 in
+  M.iter (fun x -> print_string "|" ; print_int x; print_string "|") (M.take_upto_index 1 a);
+  print_newline() ;
+  print_int (M.length a);
+  print_newline ();
+  (*print_int (M.take a);*)
+  print_newline() ;
+  print_int (M.nthq a 0);
+  Printf.printf "%B" (M.compare_till_index 0 a b) ;
+  Printf.printf "%B" (M.compare_q a b) ;
+  Printf.printf "%B" (M.check_common_el_list [2;3;4] [4;5;6]) ;
+  print_int (M.find_el 3 [6;2;3;5]);
+  M.print_list (M.prev_el_list 3 [6;2;3;5]) ;
+  M.print_list (M.maintain_order_list [1;2;3;5] [7;3;1;4;5])
 
 (* Queue *)
 let _ = U.print_header "Queue1";
