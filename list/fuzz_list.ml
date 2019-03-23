caf90787b2dece49e1f59d3b5f31ef61a02243d6
KC Sivaramakrishnan <kc@kcsrk.info>
2019-03-23 09:35:38 +0000

    Add associativity and idempotency for list

--- list/fuzz_list.ml
+++ list/fuzz_list.ml
@@ -7,8 +7,9 @@ module Atom = struct
     Int64.to_string seqno ^ ":" ^ string_of_int (Char.code c)
 
   let of_string s =
-    let [seqno; c] = String.split_on_char ':' s in
-    (Int64.of_string seqno, Char.chr (int_of_string c))
+    match String.split_on_char ':' s with
+    | [seqno; c] -> (Int64.of_string seqno, Char.chr (int_of_string c))
+    | _ -> assert false
 
   let compare (s1, c1) (s2, c2) =
     let r = Int64.compare s1 s2 in
@@ -18,9 +19,9 @@ end
 module M = Mlist.Make (Atom)
 open Crowbar
 
-type merge_data = {lca: Atom.t list; left: Atom.t list; right: Atom.t list}
+type triple_data = {lca: Atom.t list; left: Atom.t list; right: Atom.t list}
 
-type which = Left | Right | LCA
+type which3 = Left | Right | LCA
 
 let triple_gen =
   fix (fun triple_gen ->
@@ -62,7 +63,69 @@ let triple_gen =
                   (Right, seqno, {d with right= remove right})
               | _ -> state ) ] )
 
-let pp_q ppf (_, _, {left= l; right= r; lca}) =
+type quint_data =
+  { lca1: Atom.t list
+  ; lca2: Atom.t list
+  ; a: Atom.t list
+  ; b: Atom.t list
+  ; c: Atom.t list }
+
+type which5 = LCA1 | A | LCA2 | B | C
+
+let quint_gen =
+  fix (fun quint_gen ->
+      choose
+        [ const (LCA1, 0, {lca1= []; lca2= []; a= []; b= []; c= []})
+        ; (* Transition *)
+          map [quint_gen] (fun state ->
+              match state with
+              | LCA1, seqno, ({lca1; _} as d) ->
+                  (A, seqno, {d with a = lca1; lca2 = lca1})
+                  (* common prefix *)
+              | A, seqno, d -> (LCA2, seqno, d)
+              | LCA2, seqno, ({lca2; _} as d) ->
+                  (B, seqno, {d with b = lca2; c = lca2})
+                  (* common prefix *)
+              | B, seqno, d -> (C, seqno, d)
+              | _ -> state )
+        ; (* Insert *)
+          map [uint8; int32; quint_gen] (fun v idx state ->
+              let insert l seqno =
+                let idx = abs (Int32.to_int idx) mod (List.length l + 1) in
+                M.insert l idx (Int64.of_int seqno, Char.chr v)
+              in
+              match state with
+              | LCA1, seqno, ({lca1; _} as d) ->
+                  (LCA1, seqno + 1, {d with lca1 = insert lca1 seqno})
+              | LCA2, seqno, ({lca2; _} as d) ->
+                  (LCA2, seqno + 1, {d with lca2 = insert lca2 seqno})
+              | A, seqno, ({a; _} as d) ->
+                  (A, seqno + 1, {d with a = insert a seqno})
+              | B, seqno, ({b; _} as d) ->
+                  (B, seqno + 1, {d with b = insert b seqno})
+              | C, seqno, ({c; _} as d) ->
+                  (C, seqno + 1, {d with c = insert c seqno}))
+        ; (* remove *)
+          map [int32; quint_gen] (fun idx state ->
+              let remove = function
+                | [] -> []
+                | l ->
+                    let idx = abs (Int32.to_int idx) mod List.length l in
+                    M.remove l idx
+              in
+              match state with
+              | LCA1, seqno, ({lca1; _} as d) ->
+                  (LCA1, seqno, {d with lca1 = remove lca1})
+              | LCA2, seqno, ({lca2; _} as d) ->
+                  (LCA2, seqno, {d with lca2 = remove lca2 })
+              | A, seqno, ({a; _} as d) ->
+                  (A, seqno, {d with a = remove a })
+              | B, seqno, ({b; _} as d) ->
+                  (B, seqno, {d with b = remove b })
+              | C, seqno, ({c; _} as d) ->
+                  (C, seqno, {d with c = remove c })) ] )
+
+let pp_t ppf (_, _, {left= l; right= r; lca}) =
   let f (seqno, c) = pp ppf "%s:%d; " (Int64.to_string seqno) (Char.code c) in
   pp ppf "lca = [" ;
   List.iter f lca ;
@@ -72,6 +135,21 @@ let pp_q ppf (_, _, {left= l; right= r; lca}) =
   List.iter f r ;
   pp ppf "]"
 
+let pp_q ppf (_, _, {lca1; a; lca2; b; c}) =
+  let f (seqno, c) = pp ppf "%s:%d; " (Int64.to_string seqno) (Char.code c) in
+  pp ppf "lca1 = [" ;
+  List.iter f lca1 ;
+  pp ppf "], a = [" ;
+  List.iter f a ;
+  pp ppf "], lca2 = [" ;
+  List.iter f lca2 ;
+  pp ppf "], b = [" ;
+  List.iter f b ;
+  pp ppf "], c = [" ;
+  List.iter f c ;
+  pp ppf "]"
+
+
 let print_list prefix l =
   let f (seqno, c) =
     Printf.printf "%s:%d; " (Int64.to_string seqno) (Char.code c)
@@ -80,7 +158,9 @@ let print_list prefix l =
   List.iter f l ;
   print_endline "]"
 
-let triple_gen = with_printer pp_q triple_gen
+let triple_gen = with_printer pp_t triple_gen
+
+let quint_gen = with_printer pp_q quint_gen
 
 let _ =
   add_test ~name:"commutativity" [triple_gen]
@@ -94,6 +174,27 @@ let _ =
       let m2 = M.merge lca right left in
       (*     print_list "m2 = " m2; *)
       check (m1 = m2) )
+
+let _ =
+  add_test ~name:"associativity" [quint_gen]
+    (fun (_,_,{lca1; a; lca2; b; c}) ->
+      (* lca1 = LCA(a,lca2) && lca2 = LCA(b,c) *)
+      let mx = M.merge lca1 a b in
+      print_list "mx = " mx;
+      let m1 = M.merge lca1 mx c in
+      print_list "m1 = " m1;
+      let my = M.merge lca2 b c in
+      print_list "my = " my;
+      let m2 = M.merge lca1 a my in
+      print_list "m2 = " m2;
+      check (m1 = m2))
+
+let _ =
+  add_test ~name:"idempotency" [triple_gen]
+    (fun (_,_,{lca;left;right}) ->
+      let m1 = M.merge lca left right in
+      let m2 = M.merge lca m1 right in
+      check (m1 = m2))
 
 let verify_relational_property predicate diff union relate lca left right merge
     =
